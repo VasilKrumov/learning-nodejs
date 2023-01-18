c1c559a92ac82518c09991cfb475d85f2bc732d9
vasil.krumov <vassil.krumov@gmail.com>
2023-01-18 11:11:14 +0200

    ♻️ Serving HTML files in stead of flat html

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
-
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/product" METHOD="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-    )
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
