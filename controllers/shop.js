9d3610e912b00bc5427443079efecf1f7af27281
vasil.krumov <vassil.krumov@gmail.com>
2023-01-19 16:03:29 +0200

    🆕 New Model Added for the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -21,11 +22,6 @@ exports.getProduct = (req, res, next) => {
     })
 }
 
-exports.postCart = (req, res, next) => {
-    const prodId = req.body.productId
-    res.redirect('/cart')
-}
-
 exports.getIndex = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop/index', {
@@ -41,6 +37,14 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart',
     })
+}
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price)
+    })
+    res.redirect('/cart')
 }
 
 exports.getOrders = (req, res, next) => {
