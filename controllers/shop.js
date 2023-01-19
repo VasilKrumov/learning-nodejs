e0e63b1f413f2ce60f168ca91506e55204a8c385
vasil.krumov <vassil.krumov@gmail.com>
2023-01-19 14:43:33 +0200

    🏣 Passing Data with POST requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -21,6 +21,11 @@ exports.getProduct = (req, res, next) => {
     })
 }
 
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId
+    res.redirect('/cart')
+}
+
 exports.getIndex = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop/index', {
