31ec309165be83bd8eff905c9c6ac53f38543b58
vasil.krumov <vassil.krumov@gmail.com>
2023-01-25 14:51:06 +0200

    🆕 New Models & Relations Added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser')
 
 const errorController = require('./controllers/error')
 const sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express()
 
@@ -17,14 +21,39 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then((user) => {
+            req.user = user
+            next()
+        })
+        .catch((error) => console.log(error))
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(errorController.get404)
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
+
 sequelize
-    .sync()
+    .sync({ force: true })
     .then((result) => {
+        return User.findByPk(1)
+    })
+    .then((user) => {
+        if (!user) {
+            return User.create({ name: 'Max', email: 'test@email.com' })
+        }
+        return Promise.resolve(user)
+    })
+    .then((user) => {
         app.listen(3000)
     })
     .catch((error) => console.log(error, 'error'))
