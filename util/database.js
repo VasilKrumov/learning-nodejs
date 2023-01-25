db11ff6efce2609f6ceb872a3f9547820f64e105
vasil.krumov <vassil.krumov@gmail.com>
2023-01-25 15:08:22 +0200

    🎉 SQL Module Finished
    🥬 Added MongoDB in stead of SQL

--- util/database.js
+++ util/database.js
@@ -1,5 +1,13 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', 'rootpass', { dialect: 'mysql', host: 'localhost' })
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://vasil:Awesome123@cluster0.8kvabr7.mongodb.net/?retryWrites=true&w=majority')
+        .then((result) => {
+            callback(result)
+            console.log('connected')
+        })
+        .catch((err) => console.log(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
