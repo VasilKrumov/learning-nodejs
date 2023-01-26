b169c4f74b628e0e98266425c6c99ec83c5ce3d0
vasil.krumov <vassil.krumov@gmail.com>
2023-01-26 13:48:55 +0200

    🖊️ Added Edit Functionality

--- models/product.js
+++ models/product.js
@@ -1,19 +1,25 @@
-const getDb = require('../util/database').getDb
 const mongodb = require('mongodb')
+const getDb = require('../util/database').getDb
 
 class Product {
-    constructor(title, price, description, imageUrl) {
+    constructor(title, price, description, imageUrl, id) {
         this.title = title
         this.price = price
         this.description = description
         this.imageUrl = imageUrl
+        this._id = new mongodb.ObjectId(id)
     }
 
     save() {
         const db = getDb()
-        return db
-            .collection('products')
-            .insertOne(this)
+        let dbOp
+        if (this._id) {
+            // Update the product
+            dbOp = db.collection('products').updateOne({ _id: this._id }, { $set: this })
+        } else {
+            dbOp = db.collection('products').insertOne(this)
+        }
+        return dbOp
             .then((result) => {
                 console.log(result)
             })
@@ -43,8 +49,13 @@ class Product {
             .collection('products')
             .find({ _id: new mongodb.ObjectId(prodId) })
             .next()
-            .then((product) => product)
-            .catch((err) => console.log(err))
+            .then((product) => {
+                console.log(product)
+                return product
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 }
 
