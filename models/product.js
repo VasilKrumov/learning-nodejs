cbf90c4605f9da822ab3303b40b5010a4b24e63f
vasil.krumov <vassil.krumov@gmail.com>
2023-01-26 10:33:02 +0200

    🎉 Added .env correct structure and improvements

--- models/product.js
+++ models/product.js
@@ -1,16 +1,39 @@
 const getDb = require('../util/database').getDb
 
 class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(title, price, description, imageUrl) {
         this.title = title
-        this.imageUrl = imageUrl
-        this.description = description
         this.price = price
+        this.description = description
+        this.imageUrl = imageUrl
     }
 
     save() {
         const db = getDb()
-        return db.collection('products').insertOne(this)
+        return db
+            .collection('products')
+            .insertOne(this)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    static fetchAll() {
+        const db = getDb()
+        return db
+            .collection('products')
+            .find()
+            .toArray()
+            .then((products) => {
+                console.log(products)
+                return products
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 }
 
--- util/database.js
+++ util/database.js
@@ -1,12 +1,14 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
 let _db
 
 const mongoConnect = (callback) => {
-    MongoClient.connect(`${process.env.MONGO_URL}`)
+    MongoClient.connect(`${process.env.DB_LINK}`)
         .then((client) => {
-            callback(client)
+            console.log('Connected!')
             _db = client.db()
+            callback()
         })
         .catch((err) => {
             console.log(err)
