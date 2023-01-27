43d8e95bfb3003e4f930d358ae46808d2b720cab
vasil.krumov <vassil.krumov@gmail.com>
2023-01-27 14:46:12 +0200

    📈 updated couple of the routes to use mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const price = req.body.price
     const description = req.body.description
-    const product = new Product(title, price, description, imageUrl, null, req.user._id)
+    const product = new Product({ title, price, imageUrl, description })
     product
         .save()
         .then((result) => {
@@ -55,9 +55,14 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl
     const updatedDesc = req.body.description
 
-    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId)
-    product
-        .save()
+    Product.findById(prodId)
+        .then((product) => {
+            product.title = updatedTitle
+            product.price = updatedPrice
+            product.imageUrl = updatedImageUrl
+            product.description = updatedDesc
+            return product.save()
+        })
         .then((result) => {
             console.log('UPDATED PRODUCT!')
             res.redirect('/admin/products')
@@ -66,7 +71,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then((products) => {
             res.render('admin/products', {
                 prods: products,
