189e91111f7909d21ceffe54fd7d524ecdb5c785
vasil.krumov <vassil.krumov@gmail.com>
2023-01-30 14:57:06 +0200

    🍪 Cookies Work in Progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,21 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').split('=')[1]
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: false,
+        isAuthenticated: isLoggedIn,
     })
+}
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect('/')
+    // User.findById('63d3cdfea6673f35c077df57')
+    //     .then((user) => {
+    //         req.session.user = user
+    //         req.session.save((err) => {
+    //             console.log(err)
+    //         })
+    //     })
+    //     .catch((err) => console.log(err))
 }
